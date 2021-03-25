64ac9429127ea2e07c19b95e5344dfaa70620f43
prvladko <prvladko@gmail.com>
2021-03-25 16:11:20 +0300

    выдает ошибку по каждой странице

--- headhunter.py
+++ headhunter.py
@@ -7,14 +7,15 @@ st=searchVacancy&text=%D1\'%80%D0%B5%D0%BA%D1\'%80%D1\'%83%D1\'%82%D0%B5%D1%80+O
 Recruitment+%28talent+aqusition%29+OR+\'%28%D0%BF%D0%BE%D0%B4%D0%B1%D0%BE%D1\'%80%D1%83+%D0%
 BF%D0%B5%D1\'%80%D1\'%81%D0%BE%D0%BD%D0%B0%D0%BB%D0%B0%29+OR+Researcher&items_on_page={ITEMS}'''
 
-def extract_max_page():
-    headers = {
+headers = {
     'Host': 'hh.ru',
     'User-Agent': 'Safari',
     'Accept': '*/*',
     'Accept-Encoding': 'gzip, deflate, br',
     'Connection': 'keep-alive'
 }
+
+def extract_max_page():
 
     hh_request = requests.get(f'{URL}', headers = headers)
 
@@ -28,3 +29,8 @@ def extract_max_page():
         pages.append(int(page.find('a').text))
 
     return pages[-1]
+
+def extract_hh_jobs(last_page):
+    for page in range(last_page):
+        result = requests.get(f'{URL}&page = {page}', headers)
+        print(result.status_code)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from headhunter import extract_max_page
+from headhunter import extract_max_page, extract_hh_jobs
 
 max_page = extract_max_page()
-print(max_page)
+extract_hh_jobs(max_page)
