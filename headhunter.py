0611aee0af7c600ad66f91b509bf453b81580313
prvladko <prvladko@gmail.com>
2021-03-25 23:47:56 +0300

    разбил на словарь {title: company}

--- headhunter.py
+++ headhunter.py
@@ -34,6 +34,12 @@ def extract_max_page():
 
     return pages[-1] 
 
+def extract_job(html):
+    title = html.find('a').text
+    company = html.find('div', {'class': 'vacancy-serp-item__meta-info-company'}).find('a').text
+    company = company.strip()
+    return{'title': title, 'company': company}
+
 def extract_hh_jobs(last_page):
     jobs = []
     for page in range(last_page):
@@ -45,8 +51,7 @@ def extract_hh_jobs(last_page):
         soup = BeautifulSoup(result.text, 'html.parser')
         results = soup.find_all('div', {'class': 'vacancy-serp-item'})
         for result in results:
-            title = result.find('a').text
-            company = result.find('div', {'class': 'vacancy-serp-item__meta-info-company'}).find('a').text
-            print(company)
+            job = extract_job(result)
+            jobs.append(job)
         
     return jobs
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from headhunter import extract_max_page, extract_hh_jobs
 
-max_page = extract_max_page()
-extract_hh_jobs(max_page)
+hh_max_page = extract_max_page()
+hh_jobs = extract_hh_jobs(hh_max_page)
+
+print(hh_jobs)
