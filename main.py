e736efd5067999f81d94e0e38adfa7567e0762e8
prvladko <prvladko@gmail.com>
2021-03-25 14:59:12 +0300

    add BeautifulSoup module and check hh pages urls

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 headers = {
     'Host': 'hh.ru',
@@ -13,4 +14,10 @@ st=searchVacancy&text=%D1\'%80%D0%B5%D0%BA%D1\'%80%D1\'%83%D1\'%82%D0%B5%D1%80+O
 Recruitment+%28talent+aqusition%29+OR+\'%28%D0%BF%D0%BE%D0%B4%D0%B1%D0%BE%D1\'%80%D1%83+%D0%
 BF%D0%B5%D1\'%80%D1\'%81%D0%BE%D0%BD%D0%B0%D0%BB%D0%B0%29+OR+Researcher''', headers = headers)
 
-print(hh_request.text)
+hh_soup = BeautifulSoup(hh_request.text, 'html.parser')
+
+paginator = hh_soup.find('span', {'class': 'bloko-button-group'})
+
+pages = paginator.find_all('a')
+
+print(paginator)
