9c653d261206975325233736f2750d06d43852e0
prvladko <prvladko@gmail.com>
2021-03-25 22:41:05 +0300

    получаем названия вакансий

--- headhunter.py
+++ headhunter.py
@@ -35,9 +35,16 @@ def extract_max_page():
     return pages[-1] 
 
 def extract_hh_jobs(last_page):
+    jobs = []
     for page in range(last_page):
         result = requests.get('''https://hh.ru/search/vacancy?schedule=remote&items_on_page=100&search_field=name&
     st=searchVacancy&text=%D1\'%80%D0%B5%D0%BA%D1\'%80%D1\'%83%D1\'%82%D0%B5%D1%80+OR+recruiter+OR+
     Recruitment+%28talent+aqusition%29+OR+\'%28%D0%BF%D0%BE%D0%B4%D0%B1%D0%BE%D1\'%80%D1%83+%D0%
     BF%D0%B5%D1\'%80%D1\'%81%D0%BE%D0%BD%D0%B0%D0%BB%D0%B0%29+OR+Researcher''', headers = headers)
         print(result.status_code)
+        soup = BeautifulSoup(result.text, 'html.parser')
+        results = soup.find_all('div', {'class': 'vacancy-serp-item'})
+        for result in results:
+            title = result.find('a').text
+        
+    return jobs
