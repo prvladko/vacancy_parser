dc3bacf9f2656df82f3faa49a69083a682ea51cc
prvladko <prvladko@gmail.com>
2021-03-25 17:51:57 +0300

    Непричесанный код работает (без выноса ITEMS и URL в шапку)

--- headhunter.py
+++ headhunter.py
@@ -1,23 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-ITEMS = 100
-URL = '''https://hh.ru/search/vacancy?schedule=remote&search_field=name&
-st=searchVacancy&text=%D1\'%80%D0%B5%D0%BA%D1\'%80%D1\'%83%D1\'%82%D0%B5%D1%80+OR+recruiter+OR+
-Recruitment+%28talent+aqusition%29+OR+\'%28%D0%BF%D0%BE%D0%B4%D0%B1%D0%BE%D1\'%80%D1%83+%D0%
-BF%D0%B5%D1\'%80%D1\'%81%D0%BE%D0%BD%D0%B0%D0%BB%D0%B0%29+OR+Researcher&items_on_page={ITEMS}'''
-
 headers = {
-    'Host': 'hh.ru',
-    'User-Agent': 'Safari',
-    'Accept': '*/*',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Connection': 'keep-alive'
-}
+        'Host': 'hh.ru',
+        'User-Agent': 'Safari',
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive'
+    }
 
 def extract_max_page():
-
-    hh_request = requests.get(f'{URL}', headers = headers)
+    headers = {
+        'Host': 'hh.ru',
+        'User-Agent': 'Safari',
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive'
+    }
+    hh_request = requests.get('''https://hh.ru/search/vacancy?schedule=remote&items_on_page=100&search_field=name&
+    st=searchVacancy&text=%D1\'%80%D0%B5%D0%BA%D1\'%80%D1\'%83%D1\'%82%D0%B5%D1%80+OR+recruiter+OR+
+    Recruitment+%28talent+aqusition%29+OR+\'%28%D0%BF%D0%BE%D0%B4%D0%B1%D0%BE%D1\'%80%D1%83+%D0%
+    BF%D0%B5%D1\'%80%D1\'%81%D0%BE%D0%BD%D0%B0%D0%BB%D0%B0%29+OR+Researcher''', headers = headers)
 
     hh_soup = BeautifulSoup(hh_request.text, 'html.parser')
 
@@ -28,9 +31,13 @@ def extract_max_page():
     for page in paginator:
         pages.append(int(page.find('a').text))
 
-    return pages[-1]
+
+    return pages[-1] 
 
 def extract_hh_jobs(last_page):
     for page in range(last_page):
-        result = requests.get(f'{URL}&page = {page}', headers)
+        result = requests.get('''https://hh.ru/search/vacancy?schedule=remote&items_on_page=100&search_field=name&
+    st=searchVacancy&text=%D1\'%80%D0%B5%D0%BA%D1\'%80%D1\'%83%D1\'%82%D0%B5%D1%80+OR+recruiter+OR+
+    Recruitment+%28talent+aqusition%29+OR+\'%28%D0%BF%D0%BE%D0%B4%D0%B1%D0%BE%D1\'%80%D1%83+%D0%
+    BF%D0%B5%D1\'%80%D1\'%81%D0%BE%D0%BD%D0%B0%D0%BB%D0%B0%29+OR+Researcher''', headers = headers)
         print(result.status_code)
