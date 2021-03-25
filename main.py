c1e1bd1a368a91eafe57a0f0c33196139d03886c
prvladko <prvladko@gmail.com>
2021-03-25 13:36:19 +0300

    a working version of requests added

--- main.py
+++ main.py
@@ -9,16 +9,8 @@ headers = {
 }
 
 hh_request = requests.get('''https://hh.ru/search/vacancy?schedule=remote&items_on_page=100&search_field=name&
-#st=searchVacancy&text=%D1\'%80%D0%B5%D0%BA%D1\'%80%D1\'%83%D1\'%82%D0%B5%D1%80+OR+recruiter+OR+
-#Recruitment+%28talent+aqusition%29+OR+\'%28%D0%BF%D0%BE%D0%B4%D0%B1%D0%BE%D1\'%80%D1%83+%D0%
-#BF%D0%B5%D1\'%80%D1\'%81%D0%BE%D0%BD%D0%B0%D0%BB%D0%B0%29+OR+Researcher''', headers = headers)
+st=searchVacancy&text=%D1\'%80%D0%B5%D0%BA%D1\'%80%D1\'%83%D1\'%82%D0%B5%D1%80+OR+recruiter+OR+
+Recruitment+%28talent+aqusition%29+OR+\'%28%D0%BF%D0%BE%D0%B4%D0%B1%D0%BE%D1\'%80%D1%83+%D0%
+BF%D0%B5%D1\'%80%D1\'%81%D0%BE%D0%BD%D0%B0%D0%BB%D0%B0%29+OR+Researcher''', headers = headers)
 
-print(hh_request)
-
-#hh_request = requests.get
-#('''https://hh.ru/search/vacancy?schedule=remote&items_on_page=100&search_field=name&
-#st=searchVacancy&text=%D1\'%80%D0%B5%D0%BA%D1\'%80%D1\'%83%D1\'%82%D0%B5%D1%80+OR+recruiter+OR+
-#Recruitment+%28talent+aqusition%29+OR+\'%28%D0%BF%D0%BE%D0%B4%D0%B1%D0%BE%D1\'%80%D1%83+%D0%
-#BF%D0%B5%D1\'%80%D1\'%81%D0%BE%D0%BD%D0%B0%D0%BB%D0%B0%29+OR+Researcher''', headers = headers)
-
-# https://hh.ru/search/vacancy?st=searchVacancy&text=python&area=113&area=5&items_on_page=100
+print(hh_request.text)
