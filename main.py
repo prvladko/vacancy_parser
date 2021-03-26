bf7741a02a5a51fa6ea2f45a4e64ae0746db3c3e
prvladko <prvladko@gmail.com>
2021-03-26 23:58:12 +0300

    Finalized (нужно сократить ссылки по hh - оставить только id)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from so import get_jobs as so_get_jobs
 from save import save_to_csv
 
 hh_jobs = hh_get_jobs()
-#so_jobs = so_get_jobs()
+so_jobs = so_get_jobs()
 
-jobs = hh_jobs
+jobs = hh_jobs + so_jobs
 save_to_csv(jobs)
--- save.py
+++ save.py
@@ -3,7 +3,7 @@ import csv
 def save_to_csv(jobs):
     file = open('test.csv', mode='w')
     writer = csv.writer(file)
-    writer.writerow(['title', 'company', 'location', 'link'])
+    writer.writerow(['title', 'company', 'location', 'link'.encode("cp1251")])
     for job in jobs:
         writer.writerow(list(job.values()))
     return
