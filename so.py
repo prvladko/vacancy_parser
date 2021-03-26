8425073bbb467b3d7e4e2c688c115102bcc66771
prvladko <prvladko@gmail.com>
2021-03-26 15:10:57 +0300

    получение страницы StackOwerflow

--- so.py
+++ so.py
@@ -3,9 +3,13 @@ from bs4 import BeautifulSoup
 
 URL = 'https://stackoverflow.com/jobs?q=python'
 
-def extract_max_page()
+def extract_max_page():
+    request = requests.get(URL)
+    soup = BeautifulSoup(request.text, 'html.parser')
+    pages = soup.find('div', {'class': 's-pagination'}).find_all('a')
+    print(pages)
 
 def get_jobs():
     max_page = extract_max_page()
-    jobs = extract_jobs(max_page)
-    return jobs
+#    jobs = extract_jobs(max_page)
+    return []
