8ab1fdedc6ec83101ef580ab8d68d8a75870b663
prvladko <prvladko@gmail.com>
2021-03-26 23:35:49 +0300

    данные записываются в CSV в неправильном формате (кодировка не та)

--- save.py
+++ save.py
@@ -5,5 +5,5 @@ def save_to_csv(jobs):
     writer = csv.writer(file)
     writer.writerow(['title', 'company', 'location', 'link'])
     for job in jobs:
-        print(list(job.values()))
+        writer.writerow(list(job.values()))
     return
