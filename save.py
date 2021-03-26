8be541fdc1f8f8b6ccc6d24f755983a3b3554151
prvladko <prvladko@gmail.com>
2021-03-26 19:32:49 +0300

    выгрузка заголовков в CSV файл

--- save.py
+++ save.py
@@ -1,5 +1,8 @@
 import csv
 
 def save_to_csv(jobs):
-    open('test.csv', mode)
+    file = open('test.csv', mode='w')
+    writer = csv.writer(file)
+    writer.writerow(['title', 'company', 'location', 'link'])
+    print(file)
     return
