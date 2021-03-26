f49592a8faeb027e4bccb77972cf9d88d238a3bb
prvladko <prvladko@gmail.com>
2021-03-26 15:01:22 +0300

    StackOwerflow added

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from headhunter import get_jobs as hh_get_jobs
+from so import get_jobs as so_get_jobs
 
-hh_jobs = hh_get_jobs()
-print(hh_jobs)
+#hh_jobs = hh_get_jobs()
+so_jobs = so_get_jobs()
+#print(hh_jobs)
---
+++ so.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://stackoverflow.com/jobs?q=python'
+
+def extract_max_page()
+
+def get_jobs():
+    max_page = extract_max_page()
+    jobs = extract_jobs(max_page)
+    return jobs
